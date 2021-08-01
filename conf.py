8b47337b20d146848b69741ca55ffaa5a5f259a6
ATUL KUMAR <48856680+atulkumar6556@users.noreply.github.com>
2021-08-01 17:09:42 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -3,5 +3,5 @@ telegram_chat_id = "@###botremind_lock"
 # This is the bot ID of the created Telegram Bot. Paste after bot
 telegram_bot_id = "###bot1353872232:AAGIhjUTKW0mcsJ9MfKgEc3nVd9ddd4b2E8_lock"
 #threshold = 250
-ADAFRUIT_IO_KEY = '###aio_dRIL26rk7IJeZ5OkPiOtroGuCQj0_lock'
+ADAFRUIT_IO_KEY = '###aio_dRIL26rk7IJeZ5OatulkPiOtroGuCQj0_lock'
 ADAFRUIT_IO_USERNAME = '###lockadoor_lock'
